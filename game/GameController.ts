2cbc3a01a0e0d15cbf4aaea8ec7e2829c5c10732
Nate <ief015@gmail.com>
2023-06-11 12:39:39 -0400

    click+drag to launch a new body

--- game/GameController.ts
+++ game/GameController.ts
@@ -1,8 +1,14 @@
 import GameEventHandler from "~/game/GameEventHandler";
+import Point from "~/game/Point";
 import Body from "~/game/simulation/Body";
 import randomColorHex from "~/game/simulation/utils/randomColor";
 
 export default class GameController extends GameEventHandler {
+
+  private pressOrigin: Point|null = null;
+  private createMass = 1;
+  private createRadius = 4;
+  private createForce = 5;
 
   public onPreUpdate(dt: number) {
 
@@ -12,8 +18,28 @@ export default class GameController extends GameEventHandler {
 
   }
 
-  public onPreDraw(ctx: CanvasRenderingContext2D, dt: number) {
+  private drawCreateHint(ctx: CanvasRenderingContext2D) {
+    if (!this.pressOrigin)
+      return;
+    const cursor = this.getGame().getCursor();
+    const { x: originX, y: originY } = this.pressOrigin;
+    ctx.save();
+    ctx.strokeStyle = '#333';
+    ctx.lineCap = 'round';
+    ctx.beginPath();
+    ctx.lineWidth = 0.5;
+    ctx.arc(originX, originY, this.createRadius, 0, 2 * Math.PI)
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    ctx.moveTo(originX, originY);
+    ctx.lineTo(cursor.x, cursor.y);
+    ctx.stroke();
+    ctx.restore();
+  }
 
+  public onPreDraw(ctx: CanvasRenderingContext2D, dt: number) {
+    this.drawCreateHint(ctx);
   }
 
   public onPostDraw(ctx: CanvasRenderingContext2D, dt: number) {
@@ -21,18 +47,22 @@ export default class GameController extends GameEventHandler {
   }
 
   public onPressDown(x: number, y: number) {
-
+    this.pressOrigin = { x, y };
   };
 
   public onPressUp(x: number, y: number) {
-    const sim = this.getSimulation();
-    const body = new Body(x, y);
-    body.radius = 2;
-    body.mass = 1;
-    body.vx = 0;
-    body.vy = 0;
-    body.strokeColor = randomColorHex();
-    sim.getBodies().push(body);
+    if (this.pressOrigin) {
+      const { x: originX, y: originY } = this.pressOrigin;
+      const sim = this.getSimulation();
+      const body = new Body(originX, originY);
+      body.radius = this.createRadius;
+      body.mass = this.createMass;
+      body.vx = (originX - x) * this.createForce;
+      body.vy = (originY - y) * this.createForce;
+      body.strokeColor = randomColorHex();
+      sim.getBodies().push(body);
+    }
+    this.pressOrigin = null;
   };
 
 }
