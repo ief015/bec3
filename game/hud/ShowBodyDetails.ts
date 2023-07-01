f87dbf97f4720ef089af0d3b09b37914ceae83df
Nate <ief015@gmail.com>
2023-07-01 02:36:44 -0400

    stroke outline for body details

--- game/hud/ShowBodyDetails.ts
+++ game/hud/ShowBodyDetails.ts
@@ -28,17 +28,22 @@ export default class ShowBodyDetails extends GameEventHandler {
       const pos = this.getCursor();
       ctx.fillStyle = '#A6ADBA';
       ctx.font = '12px sans-serif';
-      ctx.shadowColor = '#000';
-      ctx.shadowBlur = 1.5;
-      ctx.shadowOffsetX = 0.5;
-      ctx.shadowOffsetY = 0.5;
+      ctx.shadowColor = 'black';
+      ctx.shadowBlur = 4;
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 2;
       // Draw from bottom, upwards
       ctx.translate(0, -15);
-      ctx.fillText(`R ${this.body.radius.toFixed(3)}`, pos.x, pos.y);
+      const strRadius = `R ${this.body.radius.toFixed(3)}`;
+      ctx.strokeText(strRadius, pos.x, pos.y);
+      ctx.fillText(strRadius, pos.x, pos.y);
       ctx.translate(0, -15);
-      ctx.fillText(`M ${this.body.mass.toFixed(3)}`, pos.x, pos.y);
+      const strMass = `M ${this.body.mass.toFixed(3)}`;
+      ctx.strokeText(strMass, pos.x, pos.y);
+      ctx.fillText(strMass, pos.x, pos.y);
       if (this.body.name) {
         ctx.translate(0, -15);
+        ctx.strokeText(this.body.name, pos.x, pos.y);
         ctx.fillText(this.body.name, pos.x, pos.y);
       }
       ctx.restore();
