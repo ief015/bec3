61896ee8e27dc0478c7f7ef77cae47dd82db6d67
Nate <ief015@gmail.com>
2023-07-03 01:38:59 -0400

    enable Select toolbar btn

--- components/hud/toolbar.vue
+++ components/hud/toolbar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-col gap-2">
     <div class="flex flex-row gap-2 justify-center pointer-events-auto">
-      <div class="tooltip tooltip-bottom" data-tip="Coming soon">
-        <HudToolbarBtn label="Select" name="select" v-model:selection="selection" disabled />
+      <div class="tooltip tooltip-bottom" data-tip="WIP">
+        <HudToolbarBtn label="Select" name="select" v-model:selection="selection" />
       </div>
       <div class="tooltip tooltip-bottom" data-tip="Click+drag to create bodies">
         <HudToolbarBtn label="Create" name="create" v-model:selection="selection" />
@@ -24,6 +24,7 @@
 import GameMain from '~/game/GameMain';
 import CreateToolController from '~/game/controllers/CreateToolController';
 import LookToolController from '~/game/controllers/LookToolController';
+import SelectToolController from '~/game/controllers/SelectToolController';
 
 const props = defineProps<{
   game?: GameMain;
@@ -34,8 +35,7 @@ const selection = defineModel<string>('selection', { required: true });
 watch([ () => props.game, selection ], () => {
   switch (selection.value) {
     case 'select':
-      console.warn("select not yet implemented");
-      props.game?.setController();
+      props.game?.setController(SelectToolController);
       break;
     case 'create':
       props.game?.setController(CreateToolController);
