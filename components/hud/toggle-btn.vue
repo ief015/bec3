0edfe50dd8ab148231c9e464b2aaf7b5f766a466
Nate <ief015@gmail.com>
2023-07-04 01:17:51 -0400

    HudToggleBtn

---
+++ components/hud/toggle-btn.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    class="btn btn-ghost"
+    :class="[ activeClass ]"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+
+
+const props = defineProps<{
+  active?: boolean;
+}>();
+
+const activeClass = computed(() => {
+  if (props.active) {
+    return 'btn-outline';
+  }
+  return '';
+});
+
+</script>
