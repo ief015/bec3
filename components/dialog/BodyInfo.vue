95f59e55a4b6dd434b55ce51ffc92ce2350d95b6
Nate <ief015@gmail.com>
2023-07-04 01:02:56 -0400

    wip: select > show info dialog

---
+++ components/dialog/BodyInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <HudDialog>
+    <div class="m-2">
+      <div v-for="body in bodies">
+        <div>{{ body.name }}</div>
+        <div>{{ body.mass }}</div>
+        <div>{{ body.radius }}</div>
+      </div>
+    </div>
+  </HudDialog>
+</template>
+
+<script setup lang="ts">
+import Body from '~/game/simulation/Body';
+
+defineProps<{
+  bodies?: readonly Body[];
+}>();
+
+</script>
--- components/hud/control/select.vue
+++ components/hud/control/select.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <div class="flex flex-row gap-1 justify-center">
+    <div class="flex flex-row gap-2 justify-center">
+
       <div
         class="tooltip tooltip-bottom pointer-events-auto"
         data-tip="Click+drag to select bodies"
@@ -14,6 +15,7 @@
           Select
         </button>
       </div>
+
       <div
         class="tooltip tooltip-bottom pointer-events-auto"
         data-tip="Coming soon"
@@ -28,18 +30,33 @@
           Move
         </button>
       </div>
-      <div class="border-l border-[#555]"></div>
+
+      <div class="divider divider-horizontal"></div>
+
+      <div
+        class="tooltip tooltip-bottom pointer-events-auto"
+        data-tip="Show info about selected bodies"
+      >
+        <button
+          class="btn btn-ghost btn-sm"
+          :disabled="!hasSelection"
+          @click="onClickShowInfo"
+        >
+          Info
+        </button>
+      </div>
+
       <div
         class="tooltip tooltip-bottom pointer-events-auto"
         data-tip="Coming soon"
       >
-        <button
-          class="btn btn-ghost btn-sm"
+        <button class="btn btn-ghost btn-sm"
           disabled
         >
           Edit
         </button>
       </div>
+
       <div
         class="tooltip tooltip-bottom pointer-events-auto"
         data-tip="Delete selected bodies"
@@ -52,28 +69,41 @@
           Delete
         </button>
       </div>
+
     </div>
+
     <div v-if="hasSelection" class="text-center mt-2 label-text">
       {{ selectedCount }} selected
     </div>
+
+    <DialogBodyInfo
+      v-model="showInfoDialog"
+      :bodies="selected"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import GameMain from '~/game/GameMain';
 import SelectToolController from '~/game/controllers/SelectToolController';
+import Body from '~/game/simulation/Body';
 
 const props = defineProps<{
   game: GameMain;
 }>();
 
+const showInfoDialog = ref<boolean>(false);
 const controller = ref<SelectToolController>();
 watch(() => props.game, () => {
   controller.value = props.game.getController<SelectToolController>();
 }, { immediate: true });
 
-const hasSelection = computed(() => controller.value?.hasSelection() ?? false);
-const selectedCount = computed(() => controller.value?.getSelected()?.length ?? 0);
+const hasSelection = computed<boolean>(() => controller.value?.hasSelection() ?? false);
+const selectedCount = computed<number>(() => controller.value?.getSelected()?.length ?? 0);
+const selected = computed<readonly Body[]>(() => {
+  const bodies = controller.value?.getSelected();
+  return bodies ?? [];
+});
 
 const getMode = () => {
   return 'select';
@@ -82,5 +112,9 @@ const getMode = () => {
 const onClickDelete = () => {
   controller.value?.deleteSelection();
 };
+
+const onClickShowInfo = () => {
+  showInfoDialog.value = true;
+}
 
 </script>
