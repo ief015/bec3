e15f4ae7e47360ebe08595be69c9d0b00e16eb21
Nate <ief015@gmail.com>
2023-07-04 22:44:28 -0400

    BodyInfo dialog ui update

--- components/dialog/BodyInfo.vue
+++ components/dialog/BodyInfo.vue
@@ -1,17 +1,30 @@
 <template>
   <HudDialog :title="title">
     <div v-for="(body, idx) in sortedBodies">
-      <div class="divider my-1" v-if="idx > 0"></div>
-      <ViewBodyListItem :body="body" />
+      <div class="divider" v-if="idx > 0" />
+      <ViewBodyListItem
+        :body="body"
+        :fields="bodyFields"
+        num-cols="3"
+      >
+        <template #after-title>
+          <div class="flex-grow"></div>
+          <button type="button" class="btn btn-circle btn-ghost btn-sm">
+            <EyeIcon class="h-4" />
+          </button>
+        </template>
+      </ViewBodyListItem>
     </div>
   </HudDialog>
 </template>
 
 <script setup lang="ts">
+import { FieldDefinition } from '~/components/view/BodyListItem.vue';
 import Body from '~/game/simulation/Body';
+import { EyeIcon } from '@heroicons/vue/24/outline'
 
 const props = defineProps<{
-  bodies?: readonly Body[];
+  bodies?: Body[];
 }>();
 
 const title = computed(() => {
@@ -29,5 +42,32 @@ const sortedBodies = computed(() => {
     return a.name.localeCompare(b.name);
   });
 });
+
+const bodyFields: FieldDefinition[] = [
+  {
+    label: 'Mass',
+    format: b => b.mass.toFixed(2),
+  },
+  {
+    label: 'X',
+    format: b => b.x.toFixed(2),
+  },
+  {
+    label: 'Velocity',
+    format: b => b.getSpeed().toFixed(2),
+  },
+  {
+    label: 'Radius',
+    format: b => b.radius.toFixed(2),
+  },
+  {
+    label: 'Y',
+    format: b => b.y.toFixed(2),
+  },
+  {
+    label: 'Bearing',
+    format: b => b.getDirectionDegrees().toFixed(2),
+  },
+];
 
 </script>
